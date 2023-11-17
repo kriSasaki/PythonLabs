45ddfffc87ce921c4bb4a7fe093d93fe7c770ae6
kriSasaki <99545732+kriSasaki@users.noreply.github.com>
2023-11-17 18:27:01 +0300

    Update Программирование_на_языке_Python_Лаба5.py

--- Программирование на языке Python_Лаба5/Программирование на языке Python_Лаба5/Программирование_на_языке_Python_Лаба5.py
+++ Программирование на языке Python_Лаба5/Программирование на языке Python_Лаба5/Программирование_на_языке_Python_Лаба5.py
@@ -1,16 +1,12 @@
-﻿
-
 def count_russian_chars(file_path):
     result_dict = {}
 
-    # Генерация символов русского алфавита с помощью ord() и chr()
     russian_alphabet = [chr(i) for i in range(ord('а'), ord('я') + 1)]
 
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
             content = file.read()
             
-            # Подсчет количества каждого символа русского алфавита в файле
             for char in content:
                 if char.lower() in russian_alphabet:
                     result_dict[char] = result_dict.get(char, 0) + 1
@@ -22,7 +18,6 @@ def count_russian_chars(file_path):
 
     return result_dict
 
-# Пример использования
-file_path = "C:\Games\SHEESH.txt"  # Замените на реальный путь к вашему файлу
+file_path = "C:\Games\SHEESH.txt"
 result = count_russian_chars(file_path)
 print(result)
